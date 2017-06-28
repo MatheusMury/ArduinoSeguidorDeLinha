96ef751e1ec64a4b2eb7de5456e4615af50d4a09
MatheusMury <muryfreitas12@gmail.com>
2017-06-28 16:08:41 -0300

    Add files via upload

---
+++ MoverCarrinho1.ino
@@ -0,0 +1,45 @@
+ int valEsquerda = 0;//variável para armazenar o valor analógic
+  int valMeio = 0;
+  int valDireita = 0; 
+
+  
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(4,OUTPUT);
+  int valEsquerda = 0;//variável para armazenar o valor analógic
+  int valMeio = 0;
+  int valDireita = 0; 
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+}
+
+void loop() {
+
+    
+  
+  valEsquerda = analogRead(2);// Lê o valor do sensor e o armazena na variável val.
+  valMeio = analogRead(1);
+  valDireita = analogRead(0);
+
+  digitalWrite(2,HIGH);
+  digitalWrite(4,LOW);
+
+  if(valEsquerda < 800 && valMeio< 800){
+    FrenteEsquerda();
+  }
+  else if (valDireita<800 && valMeio<800){
+    FrenteEsquerda();
+  
+
+}
+
+
+}
+void FrenteEsquerda(){
+digitalWrite(7,HIGH);
+digitalWrite(6,LOW);}
+
+ void ReEsquerda(){
+  digitalWrite(6,LOW);
+  digitalWrite(5,HIGH);
+}
---
+++ TesteDos3juntos_Hhahehe.ino
@@ -0,0 +1,22 @@
+int valEsquerda = 0;//variável para armazenar o valor analógic
+int valMeio = 0;
+int valDireita = 0; 
+void setup() 
+{
+   Serial.begin(9600);// porta serial em 9600 bps
+}
+
+void loop() 
+{
+   valEsquerda = analogRead(2);// Lê o valor do sensor e o armazena na variável val.
+   valMeio = analogRead(1);
+   valDireita = analogRead(0);
+   Serial.println("Valor Esquerda:"); // imprimir como um decimal
+   Serial.println(valEsquerda);
+   Serial.println("Valor Direita");
+   Serial.println(valDireita);
+   Serial.println("Valor Meio");  
+   Serial.println(valMeio);
+   delay(1000); // delay de 100 milissegundos antes da próxima leitura: 
+   }
+
